c3b7c53e8552564b6b1c41c1d424bc731ad26277
Alan Hou <houych@gmail.com>
2024-10-19 20:51:59 -0700

    clean up branding

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,9 +7,9 @@ import { ThemeProvider } from "@/components/custom/theme-provider";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  metadataBase: new URL("https://gemini.vercel.ai"),
-  title: "Next.js Gemini Chatbot",
-  description: "Next.js chatbot template using the AI SDK and Gemini.",
+  metadataBase: new URL("https://vercel.ai"),
+  title: "Chatbot",
+  description: "chatbot.",
 };
 
 export default async function RootLayout({
