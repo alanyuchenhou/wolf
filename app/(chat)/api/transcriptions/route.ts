7992d358b2413260e249c1a2fea5e6a750a51853
Alan Hou <houych@gmail.com>
2024-10-27 14:05:39 -0700

    Accept empty transcription as a valid  GET response (#14)

--- app/(chat)/api/transcriptions/route.ts
+++ app/(chat)/api/transcriptions/route.ts
@@ -1,17 +1,5 @@
 import { auth } from '@/app/(auth)/auth'
 
-async function getTranscription(callSid: string) {
-  const storage_service_url = process.env.GCP_STORAGE_SERVICE_URL
-  if (!storage_service_url) {
-    throw new Error('Missing required environment variables')
-  }
-  const response = await fetch(
-    `${storage_service_url}/transcriptions/${callSid}`,
-  )
-  const transcription = await response.json()
-  return { transcription }
-}
-
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url)
   const callSid = searchParams.get('callSid')
@@ -25,6 +13,12 @@ export async function GET(request: Request) {
     return Response.json('Unauthorized!', { status: 401 })
   }
 
-  const transcription = await getTranscription(callSid)
-  return Response.json(transcription)
+  const storage_service_url = process.env.GCP_STORAGE_SERVICE_URL
+  if (!storage_service_url) {
+    throw new Error('Missing required environment variables')
+  }
+  const response = await fetch(
+    `${storage_service_url}/transcriptions/${callSid}`,
+  )
+  return response
 }
