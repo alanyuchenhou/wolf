f2829728e8ba8e01bbece6873a4871628f04bbd0
Alan Hou <houych@gmail.com>
2024-11-08 11:24:39 -0800

    clean up flights related example system instructions (#23)

--- app/(chat)/api/chat/route.ts
+++ app/(chat)/api/chat/route.ts
@@ -130,18 +130,9 @@ export async function POST(request: Request) {
         - DO NOT output lists.
         - after every tool call, pretend you're showing the result to the user and keep your response limited to a phrase.
         - today's date is ${new Date().toLocaleDateString()}.
-        - if the user wants you to display agents, call the displayAgents tool.
-        - ask follow up questions to nudge user into the optimal flow
-        - ask for any details you don't know, like name of passenger, etc.'
-        - C and D are aisle seats, A and F are window seats, B and E are middle seats
-        - assume the most popular airports for the origin and destination
-        - here's the optimal flow
-          - search for flights
-          - choose flight
-          - select seats
-          - create reservation (ask user whether to proceed with payment or change reservation)
-          - authorize payment (requires user consent, wait for user to finish payment and let you know when done)
-          - display boarding pass (DO NOT display boarding pass without verifying payment)
+        - ask follow up questions to nudge user into the optimal flow.
+        - ask for any details you don't know.
+        - if the user wants you to display or list agents, call the displayAgents tool.
         '
       `,
     messages: coreMessages,
